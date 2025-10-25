0fcec18734ab8e1716b0bdafacf8696540801bdf
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2025-10-25 16:15:22 -0400

    feat: Added debug menu controller

---
+++ Assets/Scripts/UIScripts/DebugMenuController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class DebugMenuController : MonoBehaviour
+{
+
+    [SerializeField]
+    private RectTransform debugMenu;
+
+    [SerializeField]
+    private float doTweenDuration = 0.3f;
+
+    public void OpenDebugMenu(BoolEvent ctx)
+    {
+        if (ctx.Value)
+        {
+            debugMenu.DOAnchorPosY(500, doTweenDuration).SetEase(Ease.InOutCubic);
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            debugMenu.DOAnchorPosY(-510, doTweenDuration).SetEase(Ease.InOutCubic);
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+}
