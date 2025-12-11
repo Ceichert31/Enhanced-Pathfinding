7a1f0fe56bf281d12de990f4d67a63dc061509a0
Jingles5 <jonathan.ingles@mymail.champlain.edu>
2025-12-11 12:49:46 -0500

    win screen lose creen

---
+++ Assets/sceneManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SceneWin()
+    {
+        SceneManager.LoadScene("WIN");
+    }
+    public void SceneLose()
+    {
+        SceneManager.LoadScene("LOSE");
+    }
+    public void ScenePlay()
+    {
+        SceneManager.LoadScene("PLAY");
+    }
+    public void SceneMenu()
+    {
+        SceneManager.LoadScene("MENU");
+    }
+}
