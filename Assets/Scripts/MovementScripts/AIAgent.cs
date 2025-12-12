675d759e9531be56ea97c0a63f7d2c02b27bf9b9
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2025-12-11 21:03:51 -0500

    feat: Improved room modules to be more robust

--- Assets/Scripts/MovementScripts/AIAgent.cs
+++ Assets/Scripts/MovementScripts/AIAgent.cs
@@ -52,12 +52,16 @@ public class AIAgent : MonoBehaviour
 
     private LineRenderer lineRenderer;
 
+    private Animator animator;
+
     private List<Vector3> path = new();
 
     private const float CAPSULE_OFFSET = 1f;
     private const float DEBUG_LINE_OFFSET = 0.5f;
     private void Start()
     {
+        animator = GetComponentInChildren<Animator>();
+
         if (!transform.TryGetComponent(out pathfindingAlgorithm))
         {
             throw new NullReferenceException("Please add a pathfinding algorithm to the AI Agent!");
@@ -81,6 +85,8 @@ public class AIAgent : MonoBehaviour
 
     private void Update()
     {
+        animator.speed = agentSpeed;
+
         //Issue: agent position sometimes moves off navmesh, and must move to nearest navmesh point to proceed again
         path = pathfindingAlgorithm.RunPathfinding(RoundVector(transform.position), RoundVector(target.position));
 
